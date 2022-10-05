14d6753fd1ea49acaea9eb48ef1d22f7a4f64b6d
gifcolls <108479899+gifcolls@users.noreply.github.com>
2022-10-04 20:24:30 -0400

    Initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+//IIFE --Inmediately Invoked  Function Expression
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+    }
+
+    window.addEventListener("load", Start);
+})();
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+//users.js Giselle Fe 301215601 2/10/2022
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+module.exports = router;
